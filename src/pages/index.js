8e64ef6075dd763c47d5c8b945e1b64520786d9f
Alex Friedman <alexbfriedman@googlemail.com>
2021-03-30 11:17:29 +0800

    A test!

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <Layout>
       <h1>Hello from Gatsby</h1>
+      <h2>THIS IS A TEST!</h2>
       <ExampleButton>Click Me</ExampleButton>
     </Layout>
   );

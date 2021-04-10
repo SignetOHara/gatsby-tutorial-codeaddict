ef46782a0e80e961c4a505c9c0d5791b52914fb6
Alex Friedman <alexbfriedman@googlemail.com>
2021-04-10 14:00:42 +0800

    logic 1st attempt

--- src/components/Form/MainForm.js
+++ src/components/Form/MainForm.js
@@ -5,6 +5,7 @@ import FirstPage from "./Steps/FirstPage";
 import Courses from "./Steps/Courses";
 import LastPage from "./Steps/LastPage";
 import Success from "./Steps/Success";
+import Failure from "./Steps/Failure";
 // import classes from "./Form.module.scss";
 
 const MainForm = () => {
@@ -28,40 +29,49 @@ const MainForm = () => {
     watch,
   };
 
-  // function encode(data) {
-  //   return Object.keys(data)
-  //     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  //     .join("&");
-  // }
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
 
-  // const submitHandler = data => {
-  //   if (
-  //     (data.HowDidTheyKnow === null ||
-  //       data.HowDidTheyKnow === undefined ||
-  //       data.HowDidTheyKnow === false) &&
-  //     (data.other === undefined || data.other === null || data.other === "")
-  //   ) {
-  //     alert("Please choose one option or complete Other field");
-  //   } else {
-  //     // Send to Netlify Here
-  //     fetch("/", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //       body: encode({
-  //         "form-name": event.target.getAttribute("name"),
-  //         ...name,
-  //       }),
-  //     })
-  //       .then(() => setFormStep(3))
-  //       .catch(error => alert(error));
-  //   }
-  // };
+  const submitHandler = async (data, e) => {
+    if (
+      (data.HowDidTheyKnow === null ||
+        data.HowDidTheyKnow === undefined ||
+        data.HowDidTheyKnow === false) &&
+      (data.other === undefined || data.other === null || data.other === "")
+    ) {
+      alert("Please choose one option or complete Other field");
+    } else {
+      e.preventDefault();
+      // Send to Netlify Here
+      await fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({
+          // "form-name": e.target.getAttribute("Contact Form"),
+          "form-name": "Contact Form",
+          ...data,
+        }),
+      })
+        .then(res => {
+          console.log(res);
+          if (res.status === 404) {
+            setFormStep(4);
+          } else {
+            setFormStep(3);
+          }
+        })
+        .catch(error => console.log(error));
+    }
+  };
 
   return (
     <form
       name="Contact Form"
       data-netlify="true"
-      // onSubmit={handleSubmit(submitHandler)}
+      onSubmit={handleSubmit(submitHandler)}
       netlify-honeypot="bot-field"
     >
       <input type="hidden" name="bot-field" />
@@ -72,6 +82,7 @@ const MainForm = () => {
       {formStep === 1 && <Courses {...props} />}
       {formStep === 2 && <LastPage {...props} />}
       {formStep === 3 && <Success />}
+      {formStep === 4 && <Failure />}
     </form>
   );
 };
---
+++ src/components/Form/Steps/Failure.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import classes from "../Form.module.scss";
+
+// Need to make sure styling is red and correct mobile number
+const Failure = () => {
+  return (
+    <section >
+      <div >
+        <p>The form was unable to send. Please use the facebook Live chat to enroll directly or call: xxxxxx</p>
+      </div>
+    </section>
+  );
+};
+
+export default Failure;
